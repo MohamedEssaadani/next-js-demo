94c5a5609733e859e13b033704dca7024faefa75
Mohamed Es-saadani <essaadani80@gmail.com>
2022-01-10 23:32:31 +0100

    first commit

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from "react";
 
-export default function Home() {
+function about() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>About</h1>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -20,3 +18,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default about;
---
+++ pages/books/book.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function book() {
+  return (
+    <div>
+      <h1>Book </h1>
+    </div>
+  );
+}
+
+export default book;
